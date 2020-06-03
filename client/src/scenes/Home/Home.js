8c2ceccabd743984bc1b48f94ce8ad8631563477
Blaž Mandl <blaz.mandl@gmail.com>
2020-06-03 10:43:53 +0200

    Full view calendar added support for changing view of calendar

--- client/src/scenes/Home/Home.js
+++ client/src/scenes/Home/Home.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import interaction from '@fullcalendar/interaction'
 
 import './Home.scss';
 
 const Home = () => {
+    const calendarRef = useRef(null);
+
+    const handleDayClick = dateClickInfo => {
+        calendarRef.current.getApi()
+            .changeView("timeGridDay", dateClickInfo.date)
+    }
+
     return (
-        <FullCalendar defaultView="dayGridMonth" plugins={[ dayGridPlugin ]} />
+        <div className="calendar-container">
+            <FullCalendar ref={calendarRef} defaultView="dayGridMonth" plugins={[dayGridPlugin, timeGridPlugin, interaction]} height={"parent"} header={{left:'title',center:'',right:'dayGridMonth today prev,next'}}
+                dateClick={handleDayClick}
+            />
+        </div>
     )
+
 }
 
 export default Home;
