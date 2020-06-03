07c4353aef15251f095f2a46755a7757beec3a25
Blaž Mandl <MandlB@tab.pri>
2020-06-03 21:07:12 +0200

    Added toggle selectable function on change view

--- client/src/scenes/Home/Home.js
+++ client/src/scenes/Home/Home.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import timeGridPlugin from '@fullcalendar/timegrid'
@@ -8,18 +8,20 @@ import './Home.scss';
 
 const Home = () => {
     const calendarRef = useRef(null);
-
+    const [selectable, setSelectable] = useState();
     const handleDayClick = dateClickInfo => {
         calendarRef.current.getApi()
             .changeView("timeGridDay", dateClickInfo.date)
+        if (!selectable) setSelectable(true);
     }
 
     return (
         <div className="calendar-container">
-            <FullCalendar ref={calendarRef} defaultView="dayGridMonth" plugins={[dayGridPlugin, timeGridPlugin, interaction]} height={"parent"} locale="sl"
-            header={{left:'title',center:'',right:'dayGridMonth today prev,next'}} 
-            buttonText={{today: "Danes",month:"Mesec",week:"Teden",day:"Dan",list:"Seznam"}}
+            <FullCalendar ref={calendarRef} defaultView="dayGridMonth" plugins={[dayGridPlugin, timeGridPlugin, interaction]} height={"parent"} locale="sl" selectable={selectable}
+                header={{ left: 'title', center: '', right: 'dayGridMonth today prev,next' }}
+                buttonText={{ today: "Danes", month: "Mesec", week: "Teden", day: "Dan", list: "Seznam" }}
                 dateClick={handleDayClick}
+                datesRender={() => setSelectable(false)}
             />
         </div>
     )
