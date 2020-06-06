2a11a95c01c08e96edfa281319c456407f395c70
Blaž Mandl <blaz.mandl@gmail.com>
2020-06-06 12:09:54 +0200

    Added support for editing events Event adding and editing overlap validation added - events can't overlap

--- client/src/scenes/Home/Home.js
+++ client/src/scenes/Home/Home.js
@@ -4,6 +4,7 @@ import dayGridPlugin from '@fullcalendar/daygrid'
 import timeGridPlugin from '@fullcalendar/timegrid'
 import interaction from '@fullcalendar/interaction'
 import EventForm from './components/EventForm';
+import v4 from 'uuid/dist/v4';
 
 import './Home.scss';
 
@@ -12,6 +13,8 @@ const Home = () => {
     const [selectable, setSelectable] = useState();
     const [formVisible, showForm] = useState();
     const [selected, setSelected] = useState();
+    const [eventId, setEventId] = useState();
+    const [events,setEvents] = useState();
 
     useEffect(() => {
         if (!selectable) calendarRef.current.getApi().updateSize();
@@ -23,51 +26,74 @@ const Home = () => {
         if (!selectable) setSelectable(true);
     }
 
-    const handleMonthClick = (el) => {
+    const handleMonthClick = el => {
         if (el.view.type != "timeGridDay") setSelectable(false);
     }
 
     const handleTimeSelect = selectionInfo => {
         //odpiranje forme za dodajanje eventa
         setSelected({
+            title: selectionInfo.title,
             start: selectionInfo.start,
             end: selectionInfo.end,
             durationEditable: true,
             startEditable: true,
             editable: true
         });
+        setEvents(calendarRef.current.getApi().getEvents());
         showForm(true); //open form
     }
 
-    const addEvent = (formData) => {
+    const addEvent = formData => {
+        const id = eventId ? eventId : v4();
         const ref = calendarRef.current;
         const api = ref.getApi();
-        let event = { ...selected, title: formData.Namen }
-        let newStart = new Date(selected.start);    //new start from form
-        let newEnd = new Date(selected.end);    //new end from form
+
+        let newStart = new Date(formData.Datum);    //new start from form
+        let newEnd = new Date(formData.Datum);    //new end from form
         newStart.setHours(Math.floor(formData.Od / 60));
         newStart.setMinutes(formData.Od % 60);
         newEnd.setHours(Math.floor(formData.Do / 60));
         newEnd.setMinutes(formData.Do % 60);
-        event.start = newStart;
-        event.end = newEnd;
-        api.addEvent(event);
+
+        if (!eventId) { //adding new event if no event is selected for editing
+            let event = { ...selected, id, title: formData.Namen }
+            event.start = newStart;
+            event.end = newEnd;
+            api.addEvent(event);
+        }
+
+        else {  //editing existing clicked event
+            const editingEvent = api.getEventById(id);
+            editingEvent.setStart(newStart);
+            editingEvent.setEnd(newEnd);
+            editingEvent.setProp("title", formData.Namen);
+        }
         showForm(false);    //close form
+        setEventId(null);   //reset id for event (unselect event)
+    }
+
+    const handleEventClick = eventClickInfo => {
+        setEventId(eventClickInfo.event.id);
+        handleTimeSelect(eventClickInfo.event);
     }
 
     return (
         <>
             <div className="calendar-container">
-                <FullCalendar ref={calendarRef} defaultView="dayGridMonth" timezone="local" plugins={[dayGridPlugin, timeGridPlugin, interaction]} height={"parent"} locale="sl" selectable={selectable} eventOverlap={false}
+                <FullCalendar ref={calendarRef} defaultView="dayGridMonth" timezone="local" plugins={[dayGridPlugin, timeGridPlugin, interaction]} height={"parent"} locale="sl" editable={true} selectable={selectable} eventOverlap={false}
                     header={{ left: 'title', center: '', right: 'dayGridMonth today prev,next' }}
                     buttonText={{ today: "Danes", month: "Mesec", week: "Teden", day: "Dan", list: "Seznam" }}
                     dateClick={handleDayClick}
                     datesRender={handleMonthClick}
                     select={handleTimeSelect}
+                    eventClick={handleEventClick}
                 />
             </div>
             {formVisible &&
                 <EventForm date={calendarRef.current.getApi().formatIso(calendarRef.current.getApi().getDate(), { omitTime: true })}
+                    events={events}
+                    title={selected.title}
                     start={selected.start.getHours() * 60 + selected.start.getMinutes()}
                     end={selected.end.getHours() * 60 + selected.end.getMinutes()}
                     onClose={() => showForm(false)}
