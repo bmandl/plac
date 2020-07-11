3e572866a6ca2b6618d170baf6c638823c7548cd
Blaž Mandl <blaz.mandl@gmail.com>
2020-07-11 13:11:22 +0200

    Creating mvc structure

---
+++ middlewares/Authenticate.js
@@ -0,0 +1,11 @@
+const UserAuth = (() => {
+  const authenticate = () => {
+    //authenticate user with passport library
+  };
+
+  return {
+    authenticate,
+  };
+})();
+
+module.exports = UserAuth;
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const UserAuth = require('../middlewares/Authenticate');
 
 const router = express.Router();
+
+// router.use('/', UserAuth.authenticate, require('./users'));  // users route with authenticate middleware
+router.use('/', UserAuth.authenticate, require('./events')); // events route with authenticate middleware
 
 /* GET home page. */
 router.get('/', (req, res) => {
